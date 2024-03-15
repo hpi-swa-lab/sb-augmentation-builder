c80c7d8a0e51894618d10bbbf4016ec89b0a0ade
defo10 <i@dast.xyz>
2024-03-15 15:52:10 +0100

    add representation checkboxes

--- extensions/tla/tlaSequenceDiagram.js
+++ extensions/tla/tlaSequenceDiagram.js
@@ -831,6 +831,8 @@ ${transitionsAsMermaid}`;
         return mermaidOutput
     }
 
+    console.log(getMermaidOutput())
+
     useEffect(() => {
         if (mermaidContainerRef.current) {
             // mermaid adds a "data-processed" attribute to the diagram after processing it
@@ -852,10 +854,10 @@ ${transitionsAsMermaid}`;
 
 
 const State = ({ graph, initNodes }) => {
+    const config = useContext(DiagramConfig);
     const [currNode, setCurrNode] = useState(graph.nodes.get(initNodes[0].id));
     const [previewEdge, setPreviewEdge] = useState(null);
     const [prevEdges, setPrevEdges] = useState([]);
-    const config = useContext(DiagramConfig);
     const [selectedActor, setSelectedActor] = useState(config.actors[0] === "$messages" ? config.actors[1] : config.actors[0]);
     const [showMessagePayload, setShowMessagePayload] = useState(false);
     const [heightIncreaseFactor, setHeightIncreaseFactor] = useState(1);
@@ -907,11 +909,11 @@ const State = ({ graph, initNodes }) => {
 
     const ActorSelector = () => {
         return html`
-        <div style=${{ padding: "4px 4px 4px 16px" }}>
-            <label for="actor">Choose actor:</label>
+        <div style=${{ paddingLeft: "4px", display: "inline-block" }}>
             <select
                 id="actor"
                 value=${selectedActor}
+                style=${{ display: "inline-block", fontWeight: "bold", fontSize: "1.17em" }}
                 onChange=${(e) => setSelectedActor(e.target.value)}
             >
                 ${config.actors.filter(a => a !== "$messages").map((a) => html`<option value=${a}>${a}</option>`)}
@@ -937,17 +939,46 @@ const State = ({ graph, initNodes }) => {
     // a grid layout where the right column shows the StateMachines component
     // and the remaining space shows the rest
     return html`
-    <div style=${containerStyle}>
+    <div>
+        <div>
+            <style>
+            .checkbox-btn {
+                margin-right: 8px;
+            }
+            </style>
+            <h2>Representations</h2>
+            <div style=${{ display: "flex", flexDirection: "row" }}>
+                <div class="checkbox-btn">
+                    <input type="checkbox" name="sequence diagram" value="sequence" checked />
+                    <label for="sequence diagram">Sequence Diagram</label>
+                </div>
+                <div class="checkbox-btn">
+                    <input type="checkbox" name="state machine" value="state" checked />
+                    <label for="state machine">State Machine</label>
+                </div>
+                <div class="checkbox-btn">
+                    <input type="checkbox" name="table" value="table" checked />
+                    <label for="table">Table</label>
+                </div>
+                <div class="checkbox-btn">
+                    <input type="checkbox" name="text" value="text" checked />
+                    <label for="text">Text</label>
+                </div>
+            </div>
+        </div>
+        <${InitStateSelection} />
         <div style=${{ display: "grid", gridTemplateColumns: "50% 50%", height: "100%" }}>
             <div style=${containerStyle}>
-                <${InitStateSelection} />
+                <h3 style=${{ display: "inline-block" }}>Sequence Diagram</h3>
                 <${ActionHeightSlider} />
                 <${Topbar} ...${props} />
                 <${Diagram} ...${props} />
             </div >
             <div style=${{ display: "flex", flexDirection: "column", flex: "1 0 0", padding: "0 16px 0 0" }}>
-                <h4>${selectedActor}</h4>
-                <${ActorSelector} />
+                <div>
+                    <h3 style=${{ display: "inline-block" }}>State Diagram of </h3>
+                    <${ActorSelector} />
+                </div>
                 <${StateDiagram} actor=${selectedActor} currentState=${currNode} previewedState=${graph.nodes.get(previewEdge?.to)}/>
             </div>
         </div>
