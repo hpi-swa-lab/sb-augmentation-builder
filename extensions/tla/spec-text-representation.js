ccaea9cf516e1153017b42cf7d6b48530e1cf68a
Daniel <i@dast.xyz>
2024-03-25 14:47:37 +0100

    tla: improve highlights of changed vars and actions

--- extensions/tla/spec-text-representation.js
+++ extensions/tla/spec-text-representation.js
@@ -3,10 +3,88 @@ import { useContext, useEffect, useRef, useState } from "../../external/preact-h
 import { h } from "../../external/preact.mjs";
 import { shard, editor } from "../../view/widgets.js";
 import { DiagramConfig } from "./state-explorer.js";
-import { EdgePickers, exportToHTML, nestedKeys, tableHeaderStyle } from "./utils.js";
+import { EdgePickers, apply, nestedKeys } from "./utils.js";
 const html = htm.bind(h);
 
 
+const jsonToTLAString = (obj) => {
+    let json = JSON.stringify(obj);
+    if (json === undefined) return undefined;
+    // remove all " and replace : in pattern "<key>": with <key>↦
+    json = json.replace(/"(\w+)":/g, "$1↦");
+    // replace all remaining : with →
+    json = json.replace(/:/g, "→");
+    // replace all , with ,\n
+    json = json.replace(/,/g, ",\n");
+    // replace all { with [
+    json = json.replace(/{/g, "[");
+    // replace all } with ]
+    json = json.replace(/}/g, "]");
+    return json;
+}
+
+
+const exportToHTML = (keys, ...varsList) => {
+    // values is a list of primitive values or nested values
+    const values = varsList.map(vars => apply(vars, keys));
+    // each output space could have different size (e.g. a sequence with an added element)
+    // We need to find the maximum number of rows to display
+    const maxRows = Math.max(...values.map(v => Array.isArray(v) ? v.length : 1));
+
+    const rows = []
+    for (let i = 0; i < maxRows; i++) {
+        if (i === 0) {
+            const scope = keys.reduce((acc, k) => acc + "[" + k + "]");
+            const before = jsonToTLAString(Array.isArray(values[0]) ? values[0][0] : values[0])
+            rows.push(html`
+                <tr>
+                    <td>${scope}</td>
+                    ${values.map((v, j) => {
+                const current = jsonToTLAString(Array.isArray(v) ? v[i] : v)
+                const isStateUnchanged = j > 0 && before === current
+                return html`
+                        <td style=${{ "wordBreak": "break-word", opacity: isStateUnchanged ? "50%" : "100%" }}>
+                            ${current}
+                        </td>`})}
+                </tr>`);
+        } else {
+            // variable is nested (e.g. a set or sequence, represented as array)
+            const before = jsonToTLAString(Array.isArray(values[0]) ? values[0][i] : values)
+            console.log(before)
+            rows.push(html`
+                <tr>
+                    <td></td>
+                    ${values.map((v, j) => {
+                const current = jsonToTLAString(Array.isArray(v) ? v[i] : v)
+                const isStateUnchanged = j > 0 && before === current
+                return html`
+                        <td style=${{ wordBreak: "break-word", opacity: isStateUnchanged ? "50%" : "100%" }}>
+                            ${current}
+                        </td>`})}
+                </tr>`);
+        }
+    }
+
+    // add one empty row to the end. It has no vertical lines to
+    // separate the table from the rest of the content
+    rows.push(html`
+        <tr>
+            <td style=${{ border: "none" }}></td>
+            ${values.map(v => html`
+                <td style=${{ "border": "none" }}></td>`)}
+        </tr>`)
+
+    return rows;
+};
+
+
+
+const tableHeaderStyle = {
+    textAlign: "left",
+    fontWeight: "bold",
+    wordBreak: "break-word",
+    verticalAlign: "middle",
+};
 
 function SpecEditor({ currNode, setCurrNode, graph }) {
     const source = useContext(DiagramConfig).source;
@@ -27,7 +105,9 @@ function SpecEditor({ currNode, setCurrNode, graph }) {
                 const edges = enabledEdges.filter(e => e.label === actionName);
 
                 if (edges.length === 0 || !enabledEdgesNames.includes(actionName)) {
-                    return shard(node)
+                    // this action is disabled
+                    const opacity = "60%";
+                    return shard(node, { style: { opacity } })
                 }
 
                 const allReadKeys = edges.flatMap(e => nestedKeys(e.reads));
@@ -40,25 +120,34 @@ function SpecEditor({ currNode, setCurrNode, graph }) {
                 const nextNodes = edges.map(e => graph.nodes.get(e.to));
                 const nextNodesVars = nextNodes.map(n => n.vars);
 
+                const backgroundColor = "rgb(0 235 0 / 4%)";
+
                 return html`
-                <div style=${{ borderLeft: "6px solid green", marginLeft: "12px", paddingLeft: "8px" }} >
-                    <table>
-                      <thead>
-                        <tr>
-                          <th style=${tableHeaderStyle}>Scope</th>
-                          <th style=${tableHeaderStyle}>Before</th>
-                          ${edges.map(edge => html`
-                            <th>
-                                <${EdgePickers} ...${{ graph, currNode, setCurrNode, setPrevEdges: () => { }, setPreviewEdge: () => { } }} 
-                                    filterFn=${e => e === edge} />
-                            </th>`)}
-                        </tr>
-                      </thead>
-                      <tbody>
-                        ${combinedKeys.map(keySeq => exportToHTML(keySeq, currNode.vars, ...nextNodesVars))}
-                      </tbody>
-                    </table>
-                    ${shard(node, { style: { display: "block" } })}
+                <div >
+                    <div style=${{ backgroundColor }}>
+                        <table>
+                          <thead>
+                            <tr>
+                              <th style=${tableHeaderStyle}>Scope</th>
+                              <th style=${tableHeaderStyle}>Before</th>
+                              <th style=${{ ...tableHeaderStyle, colSpan: edges.length }}>After action:</th>
+                            </tr>
+                            <tr>
+                                <th style=${tableHeaderStyle}></th>
+                                <th style=${tableHeaderStyle}></th>
+                                ${edges.map(edge => html`
+                                <th>
+                                    <${EdgePickers} ...${{ graph, currNode, setCurrNode, setPrevEdges: () => { }, setPreviewEdge: () => { } }} 
+                                        filterFn=${e => e === edge} />
+                                </th>`)}
+                            </tr>
+                          </thead>
+                          <tbody>
+                            ${combinedKeys.map(keySeq => exportToHTML(keySeq, currNode.vars, ...nextNodesVars))}
+                          </tbody>
+                        </table>
+                    </div>
+                    ${shard(node, { style: { display: "block", backgroundColor } })}
                 </div>`
             };
             // FIXME not sure why this has to be in a micro task, is there global state
