a0ac715ac411779afe4ae76906e16b078bc647ab
Jan Reppien <jan.reppien@uni-bielefeld.de>
2024-04-08 17:17:40 +0200

    queryBuilder: add representation for tree with selectable nodes

--- sandblocks/query-builder/Snippit.js
+++ sandblocks/query-builder/Snippit.js
@@ -10,82 +10,90 @@ import { h } from "../../external/preact.mjs";
 
 const html = htm.bind(h);
 
+export default function Snippit({ code, tree, selectedNodes, nodeClicked }) {
+  //const [tree, setTree] = useState();
 
-export default function Snippit({code, languageGrammar}) {
-    const [tree, setTree] = useState()
-    const [selectedNodes, setSelectedNodes] = useState([])
+  //useEffect(() => {
+  //  setTree(grammar.parse(code));
+  //  console.log("set tree");
+  //}, []);
 
-    useEffect(() => {
-        setTree(languageGrammar.parse(code))
-    })
+  const TreeNode = ({ node, depth, nodeClicked, selectedNodes }) => {
+    const margin = depth == 0 ? "0px" : "30px";
+    const bg_color = selectedNodes.has(node._id) ? "coral" : "white";
 
-
-    const TreeNode = ({node, depth, nodeClicked, selectedNodes}) => {
-
-        const margin = depth == 0 ? "0px" : "30px"
-        const bg_color = selectedNodes.has(node._id) ? "coral" : "white"
-
-        //console.log("margin: " + margin + " | bg_color: " + bg_color  + " | node_type: " + node._type)
-        if(node._children != null) {
-            return html `
-                <div 
-                style=${{
-                    "margin-left": margin,
-                }}
-                >
-                <p style=${{"background-color": bg_color}} onClick=${() => nodeClicked(node._id)}>${node._type}</p>
-                ${node._children.map(child => 
-                    html`<${TreeNode} 
-                            node=${child}
-                            depth=${depth + 1}
-                            nodeClicked=${nodeClicked}
-                            selectedNodes=${selectedNodes}
-                        />`
-                    )
-                }
-                </div>`
-        } else {
-            return html``
-        }
-    }
-
-    const TreeRepresentation = ({tree, nodeClicked, selectedNodes}) => {
-        if(tree != null) {
-        return html`
-            ${tree._children.map(child => {
-            console.log("SelectedNodes: " + Array.from(selectedNodes).join(' '))
-            console.log("_id: " + child._id)
-            return html`<${TreeNode} node=${child} depth=${0} nodeClicked=${nodeClicked} selectedNodes=${selectedNodes}/>`
-            }
-            )}`
-        } else {
-        return html``
-        }
-    }
-
-
-    const nodeClicked = (id) => {
-            console.log("Clicked: " + id)
-            if(selectedNodes.has(id)) {
-            setSelectedNodes(prev => new Set([...prev].filter(x => x !== id)))
-            } else {
-            setSelectedNodes(prev => new Set([...prev, id]))
-            }
-            console.log("SelectedNodes: " + Array.from(selectedNodes).join(' '))
-        }
-
-    return html`
-        <textarea 
-            id="input"
-            name="input"
-            rows="10"
-            cols="70"
-            value=${code}
-            />
+    //console.log("margin: " + margin + " | bg_color: " + bg_color  + " | node_type: " + node._type)
+    if (node._children != null) {
+      return html` <div
+        style=${{
+          "margin-left": margin,
+        }}
+      >
+        <div
+          style=${{ display: "flex", "background-color": bg_color }}
+          onClick=${() => nodeClicked(node._id)}
+        >
+          <p>${node._type}</p>
+          <p style=${{ color: "LightGray", "margin-left": "10px" }}>
+            (id: ${node._id})
+          </p>
         </div>
-        <div class="row">
-            <${TreeRepresentation} tree=${tree} nodeClicked=${nodeClicked} selectedNodes=${selectedNodes}/>
-        </div>
-    `
+        ${node._children.map(
+          (child) =>
+            Object.hasOwn(child, "_type") &&
+            child._named &&
+            !Object.hasOwn(child, "_text") &&
+            html`<${TreeNode}
+              node=${child}
+              depth=${depth + 1}
+              nodeClicked=${nodeClicked}
+              selectedNodes=${selectedNodes}
+            />`,
+        )}
+      </div>`;
+    } else {
+      return html``;
+    }
+  };
 
+  const TreeRepresentation = ({ tree, nodeClicked, selectedNodes }) => {
+    if (tree != null) {
+      return html` ${tree._children.map((child) => {
+        //console.log("SelectedNodes: " + Array.from(selectedNodes).join(" "));
+        //console.log("_id: " + child._id);
+        return html`<${TreeNode}
+          node=${child}
+          depth=${0}
+          nodeClicked=${nodeClicked}
+          selectedNodes=${selectedNodes}
+        />`;
+      })}`;
+    } else {
+      return html``;
+    }
+  };
+
+  /*const nodeClicked = (id) => {
+    console.log("Clicked: " + id);
+    if (selectedNodes.has(id)) {
+      //setSelectedNodes((prev) => new Set([...prev].filter((x) => x !== id)));
+      selectedNodes = new Set([...selectedNodes].filter((x) => x !== id));
+    } else {
+      //setSelectedNodes((prev) => new Set([...prev, id]));
+      selectedNodes = new Set([...selectedNodes, id]);
+    }
+    console.log("SelectedNodes: " + Array.from(selectedNodes).join(" "));
+  };
+  */
+
+  return html`
+    <textarea id="input" name="input" rows="10" cols="70" value=${code} />
+    <div class="row">
+      <${TreeRepresentation}
+        tree=${tree}
+        nodeClicked=${nodeClicked}
+        selectedNodes=${selectedNodes}
+      />
+    </div>
+  `;
 }
--- sandblocks/query-builder/main.js
+++ sandblocks/query-builder/main.js
@@ -13,104 +13,108 @@ import Snippit from "./Snippit.js";
 
 const html = htm.bind(h);
 
-  function printTreeRec(tree, depth) {
-    console.log("depth",depth)
-    let spacer = ""
-    for (let i = 0; i < depth; i++) {
-      spacer += "  "
-    } 
-  
-    console.log("buffer", "" + spacer + tree._type + "\n")
-    let buffer = "" + spacer + tree._type + "\n"
-    tree._children.forEach(child => {
-      if(Object.hasOwn(child,'_type') && child._named && !Object.hasOwn(child,'_text')) {
-        buffer += printTreeRec(child,depth + 1)
-      }
-    })
-    console.log("buffer", )
-    return buffer
-  }
+const CodeCST = ({ pos, setPos, snippits, addSnippit, nodeClicked }) => {
+  console.log("pos: " + pos);
+  console.log("sinppits.length: " + snippits.length);
 
-
-
-
-
-const CodeCST = ({pos, setPos, snippits, addSnippit}) => {
-    console.log("pos: " + pos)
-    console.log("sinppits.length: " + snippits.length)
-
-    const prevButtonClick = () => {
-      if(pos != 0) {
-        setPos(pos - 1)
-      }
+  const prevButtonClick = () => {
+    if (pos != 0) {
+      setPos(pos - 1);
     }
+  };
 
-    const nextButtonClick = () => {
-      if(pos != snippits.length) {
-        setPos(pos + 1)
-      }
+  const nextButtonClick = () => {
+    console.log("pos: " + pos);
+    console.log("length: " + snippits.length);
+    if (pos >= snippits.length - 1) {
+      addSnippit();
     }
+    setPos(pos + 1);
+  };
 
-    let currentSnippit = null
+  const prevButton =
+    pos <= 0
+      ? html`<button onClick=${prevButtonClick} disabled>⬅️</button>`
+      : html`<button onClick=${prevButtonClick}>⬅️</button>`;
 
-    if(snippits.length > 0) {
-      currentSnippit = snippits[pos]
-    }
-
-
-  return html`
-    <div class="column">
-      <div style=${{display: "flex"}}>
+  return html` <div class="column">
+    <div>
+      <div style=${{ display: "flex" }}>
         <h1>Code</h1>
-        <div style=${{display: "flex"}}>
-          <button onClick=${prevButtonClick}>⬅️</button>
-          <button onClick=${nextButtonClick}>${pos != snippits.length ? "➡️" : "➕"}</button>
-        </div>
-        ${currentSnippit != null ? html`<${Snippit} code=${currentSnippit.code} grammar=${currentSnippit.grammar}/>` : html``}
+        ${prevButton}
+        <p>${pos + 1} / ${snippits.length}</p>
+        <button onClick=${nextButtonClick}>
+          ${pos >= snippits.length - 1 ? "➕" : "➡️"}
+        </button>
       </div>
-    </div>`
+      ${snippits.map((snippit, index) => {
+        if (index == pos) {
+          return html`<${Snippit}
+            code=${snippit.code}
+            tree=${snippit.tree}
+            grammar=${snippit.grammar}
+            selectedNodes=${snippit.selectedNodes}
+            nodeClicked=${nodeClicked}
+          />`;
+        }
+      })}
+    </div>
+  </div>`;
 };
-
 
 export function QueryBuilder() {
   const typescript = languageFor("typescript");
-  //const [inputContent, setInputContent] = useState(''); 
-  //const [queryPatternContent, setQueryPatternContent] = useState(''); 
-  //const [queryConditionContent, setQueryConditionContent] = useState(''); 
-  //const [tree, setTreeContent] = useState()
-  const [pos,setPos] = useState(0)
-  //const [treeString,setTreeStringContent] = useState("")
-  const [selectedNodes, setSelectedNodes] = useState(new Set())
-  const [snippits, setSnippits] = useState([])
-
-  const onInputChange = useCallback((e) => {
-    //console.log("Input", e);
-    //let tree = typescript.parse(e)
-    //setTreeContent(tree)
-    //console.log(tree;
-    //setTreeStringContent(printTreeRec(tree,0))s
-    //setInputContent(e)
-  }, []);
+  const [pos, setPos] = useState(-1);
+  const [snippits, setSnippits] = useState([]);
 
   useAsyncEffect(async () => {
     await typescript.ready();
-    setSnippits((snippits) => [...snippits, {code: "const x = 1", grammar: typescript}])
-    console.log("Set Example")
-  //  console.log(typescript.parse(`const x = 1;`));
   }, []);
 
-  const addSnippits = (snippits) => {
-    setSnippits()
-  }
+  const nodeClicked = (id) => {
+    console.log("NodeClicked with id: " + id);
+    setSnippits((snippits) =>
+      snippits.map((snippit, index) => {
+        console.log("index: " + index);
+        console.log(snippit);
+        if (index == pos) {
+          if (snippit.selectedNodes.has(id)) {
+            snippit.selectedNodes = new Set(
+              [...snippit.selectedNodes].filter((x) => x !== id),
+            );
+          } else {
+            snippit.selectedNodes = new Set([...snippit.selectedNodes, id]);
+          }
+        }
+        console.log(snippit);
+        return snippit;
+      }),
+    );
+  };
+  //const nodeClicked = (id) => console.log("id: " + id + " clicked");
+
+  const addSnippit = () => {
+    const tree = typescript.parse("const x = 1");
+    setSnippits((snippits) => [
+      ...snippits,
+      {
+        code: "const x = 1",
+        tree: tree,
+        selectedNodes: new Set(),
+        nodeClicked: nodeClicked,
+      },
+    ]);
+  };
 
   return html`
     <body>
       <div style="display: flex">
         <${CodeCST}
-        pos=${pos}
-        setPos=${setPos}
-        snippits=${snippits}
-        addSnippits=${addSnippits}
+          pos=${pos}
+          setPos=${setPos}
+          snippits=${snippits}
+          addSnippit=${addSnippit}
+          nodeClicked=${nodeClicked}
         />
       </div>
     </body>
