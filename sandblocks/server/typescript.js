27aebe73a42d265b256cdbd4a47e6d6853b1cf68
Tom Beckmann <tomjonabc@gmail.com>
2024-05-04 18:11:00 +0200

    add typescript support

---
+++ sandblocks/server/typescript.js
@@ -0,0 +1,20 @@
+import { transformFile } from "@swc/core";
+import * as fsPath from "path";
+
+export function typescript(app, rootPath) {
+  app.get(/\/.*\.ts$/, async (req, res) => {
+    const relPath = req.url.slice(1).split("?")[0];
+    const path = fsPath.join(rootPath, relPath);
+    const r = await transformFile(path, {
+      jsc: {
+        target: "es2016",
+        parser: {
+          syntax: "typescript",
+          tsx: path.endsWith(".tsx"),
+        },
+      },
+      sourceMaps: "inline",
+    });
+    res.setHeader("Content-Type", "text/javascript").send(r.code);
+  });
+}
