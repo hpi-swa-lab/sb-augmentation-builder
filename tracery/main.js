fa2065095b62af11e389bc229c3143bf78c3bf1e
Tom Beckmann <tomjonabc@gmail.com>
2024-06-24 10:37:56 +0200

    tracery: remove hard-coded paths

--- tracery/main.js
+++ tracery/main.js
@@ -3,6 +3,8 @@ import { FileProject } from "./project.js";
 import { appendCss, matchesKey } from "../utils.js";
 import { openBrowser } from "./browser.ts";
 import { openSearch } from "./search.js";
+import { useAsyncEffect, useLocalStorageSignal } from "../view/widgets.js";
+import { useMemo } from "../external/preact-hooks.mjs";
 
 appendCss(`
 html {
@@ -10,29 +12,69 @@ html {
   font-size: 13px;
 }`);
 
-const project = new FileProject({ folder: "/home/tom/Code/squeak/sb-js" });
-await project.open();
+function Tracery() {
+  const projectPath = useLocalStorageSignal("project-path", null);
+  const startUpFile = useLocalStorageSignal("start-up-file", null);
+  const project = useMemo(
+    () =>
+      projectPath.value ? new FileProject({ folder: projectPath.value }) : null,
+    [projectPath.value],
+  );
 
-openBrowser(
-  project,
-  {
-    initialSelection: {
-      path: "/home/tom/Code/squeak/sb-js/tracery/augmentation-builder.ts",
-      topLevel: "augmentationBuilder",
-    },
-  },
-  {
-    doNotStartAttached: true,
-    initialPosition: { x: 100, y: 100 },
-    initialSize: { x: 800, y: 600 },
-  },
-);
+  useAsyncEffect(async () => {
+    if (project) {
+      await project.open();
+
+      openBrowser(
+        project,
+        {
+          initialSelection: startUpFile.value
+            ? {
+                path: project.path + "/" + startUpFile.value.split(":")[0],
+                topLevel: startUpFile.value.split(":")[1],
+              }
+            : undefined,
+        },
+        {
+          doNotStartAttached: true,
+          initialPosition: { x: 100, y: 100 },
+          initialSize: { x: 800, y: 600 },
+        },
+      );
+    }
+  }, [project]);
+
+  return [
+    h(
+      "button",
+      {
+        onclick: () => {
+          const path = prompt("Enter project path", projectPath.value ?? "");
+          if (path) projectPath.value = path;
+        },
+      },
+      "Set Project Path",
+    ),
+    h(
+      "button",
+      {
+        onclick: () => {
+          const val = prompt(
+            "Enter Start Up Path (relativePath:symbolName)",
+            startUpFile.value ?? "",
+          );
+          if (val) startUpFile.value = val;
+        },
+      },
+      "Set Start Up Path",
+    ),
+    projectPath.value &&
+      h("button", { onclick: () => openBrowser(project) }, "Open Browser"),
+  ];
+}
 
 const editor = document.createElement("div");
-render(
-  h("button", { onclick: () => openBrowser(project) }, "Open Browser"),
-  editor,
-);
+render(h(Tracery), editor);
 document.body.appendChild(editor);
 
 document.body.addEventListener("keydown", (e) => {
