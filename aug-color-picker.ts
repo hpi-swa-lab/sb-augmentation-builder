6e91d651bd0db8a8044eaa5f91473e25f59ccede
= <=>
2024-07-15 20:56:08 +0200

    augmentationBuider: add editing option to color picker

--- aug-color-picker.ts
+++ aug-color-picker.ts
@@ -16,25 +16,32 @@ export const augExample = {
   model: languageFor("typescript"),
   match: (it) =>
     metaexec(it, (capture) => [
+      query('"$content"'),
+      (it) => it.content,
+      capture("node"),
+      (it) => it.text,
       first(
         [
-          query('"$content"'),
-          (it) => it.content,
-          first(
-            [query("rgb($r,$g,$b)")],
-            [
-              (it) => it.text,
-              (it) => /^#[0-9A-F]{6}$/i.test(it),
-              (it) => /#([a-z0-9]{2})([a-z0-9]{2})([a-z0-9]{2})/i.exec(it),
-              (it) => ({
-                r: parseInt(it[1], 16),
-                g: parseInt(it[2], 16),
-                b: parseInt(it[2], 16),
-              }),
-            ],
-          ),
+          (it) => /rgb\([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]{3}\)/i.test(it),
+          (it) =>
+            /rgb\(([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]),([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]),([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\)/i.exec(
+              it,
+            ),
+          (it) => ({
+            r: parseInt(it[1], 10),
+            g: parseInt(it[2], 10),
+            b: parseInt(it[3], 10),
+          }),
         ],
-        [query("{r: $r, g: $g, b: $b")],
+        [
+          (it) => /^#[0-9A-F]{6}$/i.test(it),
+          (it) => /#([a-z0-9]{2})([a-z0-9]{2})([a-z0-9]{2})/i.exec(it),
+          (it) => ({
+            r: parseInt(it[1], 16),
+            g: parseInt(it[2], 16),
+            b: parseInt(it[3], 16),
+          }),
+        ],
       ),
       all(
         [(it) => it.r, capture("r")],
@@ -42,7 +49,18 @@ export const augExample = {
         [(it) => it.b, capture("b")],
       ),
     ]),
-  view: ({ nodes }) => h("div", {}, "test"),
+  view: ({ r, g, b, node }) =>
+    h(
+      "div",
+      {},
+      h("input", {
+        type: "color",
+        onchange: (e) => node.replaceWith(e.target.value, "expression"),
+        value: `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`,
+      }),
+    ),
   rerender: () => true,
-  examples: [['const a = "#123456"', [0, 0]]],
+  examples: [['const a = "rgb(12,34,56)"', [0, 0]]],
 };
+
+//const a = "#123456"
