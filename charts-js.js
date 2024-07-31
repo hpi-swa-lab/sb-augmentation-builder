45a390fcbe72350d5ab7fff24a9985f9d0139cb5
= <=>
2024-07-31 12:02:36 +0200

    Augmentation example: add way to dynamically find data used for charts

--- charts-js.js
+++ charts-js.js
@@ -1,20 +1,25 @@
 const charts = () => {
+  const month = [
+    [1, 2, 3],
+    [3, 4, 5],
+  ];
+
   const data = {
     labels: ["Jan", "Feb", "Mar"],
     datasets: [
       {
         label: "Fully Rounded",
-        data: [1, 2, 3],
+        data: month[0],
         borderColor: "#1111ffff",
-        backgroundColor: "rgb(0,0,255, 0.5)",
+        backgroundColor: "rgb(1,1,255,0.5)",
         borderWidth: 2,
         borderRadius: 200,
         borderSkipped: false,
       },
       {
         label: "Small Radius",
-        data: [4, 5, 6],
-        borderColor: "rgb(255,0,128)",
+        data: month[1],
+        borderColor: "rgb(255,1,128)",
         backgroundColor: "rgb(128,128,128,0.5)",
         borderWidth: 2,
         borderRadius: 5,
